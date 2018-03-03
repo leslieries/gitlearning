569c73280042e8dcb82b1bab717ea5bca8ecc3cd
eal109 <eal109@georgetown.edu>
2018-03-03 12:21:44 -0500

    EL: Deleted line without data, adding columns

--- testTX.R
+++ testTX.R
@@ -6,13 +6,15 @@ rawdata<-read.csv("JNandMLMPAustHouDall.csv", header=T, colClasses=c("factor",  
 #rawdata[,c(4:9)]<-as.numeric((rawdata[,c(4:9)]))
 rawdata$cumJan1<-rawdata$Jan1.15
 rawdata$cumJan16<-sum(rawdata$Jan1.15,rawdata$Jan16.31)
-rawdata$cumFeb1<-sum(rawdata$cuJan16,rawdata$Feb1.15)
-rawdata$cumFeb16<-sum(rawdata$Jan1.15,rawdata$Jan16.31)
+rawdata$cumFeb1<-sum(rawdata$cumJan16,rawdata$Feb1.15)
+rawdata$cumFeb16<-sum(rawdata$cumFeb1,rawdata$Feb16.28)
+rawdata$cumMar1<-sum(rawdata$cumFeb16,rawdata$Mar1.15)
+rawdata$cumMar16<-sum(rawdata$Mar1,rawdata$Mar16.31)
 
-cumFeb1
-cumFeb16
-cumMar1
-cumMar16
+#cumFeb1
+#cumFeb16
+#cumMar1
+#cumMar16
 
 
 
