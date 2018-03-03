b7bb7044a463401d25df02dac296800f90741c77
eal109 <eal109@georgetown.edu>
2018-03-03 12:25:43 -0500

    Corrected cum. calculations

--- testTX.R
+++ testTX.R
@@ -5,11 +5,11 @@ rawdata<-read.csv("JNandMLMPAustHouDall.csv", header=T, colClasses=c("factor",  
 
 #rawdata[,c(4:9)]<-as.numeric((rawdata[,c(4:9)]))
 rawdata$cumJan1<-rawdata$Jan1.15
-rawdata$cumJan16<-sum(rawdata$Jan1.15,rawdata$Jan16.31)
-rawdata$cumFeb1<-sum(rawdata$cumJan16,rawdata$Feb1.15)
-rawdata$cumFeb16<-sum(rawdata$cumFeb1,rawdata$Feb16.28)
-rawdata$cumMar1<-sum(rawdata$cumFeb16,rawdata$Mar1.15)
-rawdata$cumMar16<-sum(rawdata$Mar1,rawdata$Mar16.31)
+rawdata$cumJan16<-rawdata[,4]+rawdata[,5]
+rawdata$cumFeb1<-rawdata[,4]+rawdata[,5]+rawdata[,6]
+rawdata$cumFeb16<-rawdata[,4]+rawdata[,5]+rawdata[,6]+rawdata[,7]
+rawdata$cumMar1<-rawdata[,4]+rawdata[,5]+rawdata[,6]+rawdata[,7]+rawdata[,8]
+rawdata$cumMar16<-rawdata[,4]+rawdata[,5]+rawdata[,6]+rawdata[,7]+rawdata[,8]+rawdata[,9]
 
 #cumFeb1
 #cumFeb16
