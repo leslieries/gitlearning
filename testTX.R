e8e11ca17c33901c255917f918f46b5a5200617a
Leslie Ries <lr354@georgetown.mei.georgetown.edu>
2018-03-03 12:06:05 -0500

    adding all the project info

---
+++ testTX.R
@@ -0,0 +1,25 @@
+#Exploratory Texas Stuff
+#2018-02
+setwd("C:/Users/lr354/Desktop")
+
+rawdata<-read.csv("JNandMLMPAustHouDall.csv", header=T, colClasses=c("factor",  "factor", "numeric","numeric","numeric","numeric","numeric","numeric"))
+
+#rawdata[,c(4:9)]<-as.numeric((rawdata[,c(4:9)]))
+rawdata$cumJan1<-rawdata$Jan1.15
+rawdata$cumJan16<-sum(rawdata$Jan1.15,rawdata$Jan16.31)
+rawdata$cumFeb1<-sum(rawdata$cuJan16,rawdata$Feb1.15)
+rawdata$cumFeb16<-sum(rawdata$Jan1.15,rawdata$Jan16.31)
+
+cumFeb1
+cumFeb16
+cumMar1
+cumMar16
+
+
+
+
+txNDVI<-rawdata[rawdata$Metric=="NDVI",]
+jnAdult<-rawdata[rawdata$Metric=="JN-Adult",]
+jnEgg<-rawdata[rawdata$Metric=="JN-Egg",]
+jnMWeed<-rawdata[rawdata$Metric=="JN-MW",]
+
